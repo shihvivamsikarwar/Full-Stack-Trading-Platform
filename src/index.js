892d0f32a832eb88b42223553d89f80cc4fb9962
Shivam Sikarwar <163384691+shihvivamsikarwar@users.noreply.github.com>
2026-01-08 21:45:29 +0530

    SignUp Page first Commit

--- src/index.js
+++ src/index.js
@@ -3,14 +3,23 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import HomePage from "./landing_page/home/HomePage";
+import Navbar from "./landing_page/Navbar";
+import Footer from "./landing_page/Footer";
+import Signup from "./landing_page/signup/Signup";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <Navbar />
     <Routes>
-      <Route path="/" element={<HomePage />}>
-        {" "}
-      </Route>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<Signup />} />
+      {/* <Route path="/" element={<About />} />
+      <Route path="/" element={<Pricing />} />
+      <Route path="/" element={<Products />} />
+      <Route path="/" element={<Support />} />
+      <Footer /> */}
     </Routes>
+    <Footer />
   </BrowserRouter>
 );
