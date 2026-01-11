9fa6d09e8d25c7f9068323100533d45683246675
Shivam Sikarwar <163384691+shihvivamsikarwar@users.noreply.github.com>
2026-01-11 21:49:04 +0530

    AboutPage Completed

--- src/landing_page/about/Hero.js
+++ src/landing_page/about/Hero.js
@@ -7,28 +7,54 @@ function Hero() {
           We pioneered the discount broking model in India. <br />
           Now, we are breaking ground with our technology.
         </h1>
-        <div className="col-6 mt-5">
+        <div
+          className="col-6 mt-5 text-muted"
+          style={{
+            fontSize: "1rem",
+            linelength: "1.8",
+            marginBottom: "15px",
+            textAlign: "left",
+          }}
+        >
           <p className="mt-5">
             We kick-started operations on the 15th of August, 2010 with the goal
             of breaking all barriers that traders and investors face in India in
             terms of cost, support, and technology. We named the company
             Zerodha, a combination of Zero and "Rodha", the Sanskrit word for
-            barrier. Today, our disruptive pricing models and in-house
-            technology have made us the biggest stock broker in India. Over 1.6+
-            crore clients place billions of orders every year through our
-            powerful ecosystem of investment platforms, contributing over 15% of
-            all Indian retail trading volumes.
+            barrier.
+          </p>
+          <p>
+            Today, our disruptive pricing models and in-house technology have
+            made us the biggest stock broker in India
+          </p>
+          <p>
+            Over 1.6+ crore clients place billions of orders every year through
+            our powerful ecosystem of investment platforms, contributing over
+            15% of all Indian retail trading volumes.
           </p>
         </div>
-        <div className="col-6 mt-5">
+        <div
+          className="col-6 mt-5 text-muted"
+          style={{
+            fontSize: "1rem",
+            lineHeight: "1.8",
+            marginBottom: "15px",
+            textAlign: "left",
+          }}
+        >
           <p className="mt-5">
             In addition, we run a number of popular open online educational and
-            community initiatives to empower retail traders and investors.
+            community initiatives to empower retail traders and investors
+          </p>
+          <p>
             Rainmatter, our fintech fund and incubator, has invested in several
             fintech startups with the goal of growing the Indian capital
-            markets. And yet, we are always up to something new every day. Catch
-            up on the latest updates on our blog or see what the media is saying
-            about us or learn more about our business and product philosophies.
+            markets.
+          </p>
+          <p>
+            And yet, we are always up to something new every day. Catch up on
+            the latest updates on our blog or see what the media is saying about
+            us or learn more about our business and product philosophies.
           </p>
         </div>
       </div>
--- src/landing_page/about/Team.js
+++ src/landing_page/about/Team.js
@@ -1,5 +1,107 @@
 import React from "react";
 function Team() {
-  return <h1>Team</h1>;
+  return (
+    <div className="container">
+      <div className="row">
+        <h3 className="text-center">People</h3>
+        <div className="col-6 mt-5 text-center">
+          <img
+            src="media/images/Sikshi_pic.png"
+            className="rounded-circle mt-5"
+          ></img>
+          <h4 className=" mt-3 fs-5">Shivam Sikarwar</h4>
+          <p className="text-muted">Founder, CEO</p>
+        </div>
+        <div className="col-6 mt-5">
+          <p className="mt-5 fs-5">
+            Shivam bootstrapped and founded Zerodha in 2026 to overcome the
+            hurdles he faced during his decade long stint as a trader. Today,
+            Zerodha has changed the landscape of the Indian broking industry.
+          </p>
+          <p className="fs-5">
+            He is a member of the SEBI Secondary Market Advisory Committee
+            (SMAC) and the Market Data Advisory Committee (MDAC).
+          </p>
+          <p className="fs-5">Playing basketball is his zen.</p>
+          <p className="fs-5">
+            Connect on{" "}
+            <a href="" style={{ textDecoration: "none" }}>
+              Homepage
+            </a>{" "}
+            /{" "}
+            <a href="" style={{ textDecoration: "none" }}>
+              TradingQnA
+            </a>{" "}
+            /{" "}
+            <a href="" style={{ textDecoration: "none" }}>
+              Twitter
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className="row text-center mt-5">
+        <div className="col-4">
+          <img
+            src="media/images/Sikshi_pic.png"
+            className="rounded-circle mt-5"
+          ></img>
+          <h4 className=" mt-3 fs-5">Shivam Sikarwar</h4>
+          <p className="text-muted">Founder, CEO</p>
+        </div>
+        <div className="col-4">
+          <img
+            src="media/images/Sikshi_pic.png"
+            className="rounded-circle mt-5"
+          ></img>
+          <h4 className=" mt-3 fs-5">Shivam Sikarwar</h4>
+          <p className="text-muted">Founder, CEO</p>
+        </div>
+        <div className="col-4">
+          <img
+            src="media/images/Sikshi_pic.png"
+            className="rounded-circle mt-5"
+          ></img>
+          <h4 className=" mt-3 fs-5">Shivam Sikarwar</h4>
+          <p className="text-muted">Founder, CEO</p>
+        </div>
+      </div>
+      <div className="row text-center mt-5">
+        <div className="col-4">
+          <img
+            src="media/images/Sikshi_pic.png"
+            className="rounded-circle mt-5"
+          ></img>
+          <h4 className=" mt-3 fs-5">Shivam Sikarwar</h4>
+          <p className="text-muted">Founder, CEO</p>
+        </div>
+        <div className="col-4">
+          <img
+            src="media/images/Sikshi_pic.png"
+            className="rounded-circle mt-5"
+          ></img>
+          <h4 className=" mt-3 fs-5">Shivam Sikarwar</h4>
+          <p className="text-muted">Founder, CEO</p>
+        </div>
+        <div className="col-4">
+          <img
+            src="media/images/Sikshi_pic.png"
+            className="rounded-circle mt-5"
+          ></img>
+          <h4 className=" mt-3 fs-5">Shivam Sikarwar</h4>
+          <p className="text-muted">Founder, CEO</p>
+        </div>
+      </div>
+      <div className="row text-center mt-5 mb-5">
+        <div className="col-4">
+          <img
+            src="media/images/Sikshi_pic.png"
+            className="rounded-circle mt-5"
+          ></img>
+          <h4 className=" mt-3 fs-5">Shivam Sikarwar</h4>
+          <p className="text-muted">Founder, CEO</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Team;
